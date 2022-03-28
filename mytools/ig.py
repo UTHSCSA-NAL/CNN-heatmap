e51d8f130d42ad88eb87fb1d26cc40eb0c620077
DWang <jieyoushen@gmail.com>
2022-03-28 11:55:05 -0500

    Update ig.py

--- mytools/ig.py
+++ mytools/ig.py
@@ -21,15 +21,13 @@ def integrated_gradients(input_, model, target_class, baseline_name, steps, cuda
 	elif baseline_name == "g":
 		baseline = inputs + torch.randn(1, 1, 193, 229, 193)       # gaussian
 	 
-	# ([51，1, 1, 193, 229, 193])
 	interpolated_inputs = [baseline + (i / steps) *(input_ - baseline) for i in range(0, steps + 1)] 
    
 	gradients = []
 	for img in interpolated_inputs:
-		
 		#img = torch.Size([1, 193, 229, 193])
 		img = torch.tensor(img, dtype=torch.float32, device=device, requires_grad=True)
-		outputs = model(img)  # get output of img ex: [0.0002, 0.9068]
+		outputs = model(img)  
 		
 		model.zero_grad()
 		outputs[:,target_class.item()].backward()
@@ -37,7 +35,7 @@ def integrated_gradients(input_, model, target_class, baseline_name, steps, cuda
 		gradients.append(gradient)
 		
 	gradients = np.array(gradients)
-	avg_grads = np.average(gradients[:-1], axis=0)                            # average image gradient
+	avg_grads = np.average(gradients[:-1], axis=0)   # average image gradient
 
 	diff_input = (input_ - baseline).detach().squeeze(0).cpu().numpy()
 	integrated_grad = diff_input * avg_grads

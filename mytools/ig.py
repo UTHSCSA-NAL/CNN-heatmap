d51e7401eec19d34caf17cb744370bc6291607a6
Di Wang <jieyoushen@gmail.com>
2022-06-13 11:30:27 -0500

    Update ig.py

--- mytools/ig.py
+++ mytools/ig.py
@@ -13,18 +13,34 @@ import pathlib
 import sys
 torch.cuda.empty_cache() 
 
-def integrated_gradients(input_, model, target_class, baseline_name, steps, cuda=True, device):
+def integrated_gradients(input_, model, target_class, baseline_name ="z", steps = 100, cuda=True, device = 0):
+	"""
+	Pythorch implementation of  integrated gradients(IG) function. IG was introduced by Sundararajan et al.,2017 (https://arxiv.org/abs/1703.01365),
+	aiming at calculating a heatmap that shows regions that contribute to the decision of a CNN model.
+	
+	Args:
+		input_: input image to get IG heatmap.
+		model: pretrained CNN model 
+		target_class: target class of imput image
+		baseline_name: select baseline image, default is zero image with same size as imput image.
+		steps: steps used to interpolated input images, default is 100
+		device: gpu device, default = 0
+	"""	
+	
 	input_size = input_.shape
 	
+	# select baseline image
 	if baseline_name == "z":                                           # zero baseline
 		baseline = np.zeros(input_size)
 	elif baseline_name == "u":                                         # uniform noise baseline
 		baseline = torch.rand(input_size)                    
 	elif baseline_name == "g":                                         # add gaussian noise baseline
 		baseline = input_ + torch.randn(input_size)       
-		
+	
+	# generating interpolated images 
 	i_images = [ baseline + (i / steps) *(input_ - baseline) for i in range(0, steps + 1)] 
- 
+ 	
+	# calculating the mean gradient of all interpolated images
 	all_grad = []
 	for img in i_images:
 		img = torch.tensor(img, dtype=torch.float32, device=device, requires_grad=True)
